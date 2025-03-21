8d1196dd4af23d17fe7f629c6306ddbceb523958
kumarkeshav09 <kumarkeshavkashyap7@gmail.com>
2025-03-21 11:53:29 +0530

    abou page

---
+++ src/pages/about/page.js
@@ -0,0 +1,11 @@
+const AboutPage = () => {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>Some information about the site.</p>
+      </div>
+    );
+  };
+  
+  export default AboutPage;
+  
